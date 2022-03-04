739190f8a36c1da92302a6d2017180f786fbab45
mr-shadow-ron <100722912+mr-shadow-ron@users.noreply.github.com>
2022-03-04 14:05:21 -0600

    Add files via upload

---
+++ Funciones basicas c++.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //Para escribir alguna libreria primero se tiene que poner numeral, seguido de include y el nombre de la libreria, en este caso Iostream (Input & Output Stream o Entrada y salida de datos) se escribe de la siguiente forma: #Inlcude<"nombre de libreria">
+using namespace std; //Sus siglas significan character input y es una de las las principales herramientas
+
+int main () { //El int main indica el punto de ejecucion de cualquier programa
+
+	int Numero = 15; //int se utiliza   junto al nombre de una variable Ejemplo: "Numero". Para poder mostrar numeros enteros. Int + Variable = (numero que se desea agregar)
+		float Decimal = 13.20;//float se utiliza para poner numeros con puntos decimales no superiores a centecimas
+			double decima2 =13.34567; //double se utiliza para poner numeros con puntos decimales no superiores a diezmilesimas, si sobre pasa esto se redondeará o saltara un error a la pantalla
+ 				char caracter="?"; //Se utiliza principalmente para mostrar caracteres, sin importar cual sea su tipo.la sintaxis es= char"nombre de variable"="x";
+	 cout <<decima2; //Al hacer esto se mostrará en pantalla cualquier funcion al momento de correr el programa su sintaxis es= cout<<nombre de variable; (SI se quiere mostrar un mensaje especifico la sintaxys es: cout <<"Frase que pondras";
+	return 0;
+}
+//Dudas o contacto: Ig: Mr_Shxdxw_Rxn Numero: +52 33 27204951
